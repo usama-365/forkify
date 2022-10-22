e531616223d52c267207b39d25872b6e8ab1dfca
usama-365 <usama-365@protonmail.com>
2022-10-22 14:38:15 +0500

    Feature added to render a recipe with loading spinner

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -5,7 +5,17 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const response = await fetch("https://forkify-api.herokuapp.com/api/get?rId=47746");
+        const data = await response.json();
+        console.log(response, data);
+    } catch (error) {
+        alert(error);
+    }
+};
 
 //# sourceMappingURL=index.62406edb.js.map
